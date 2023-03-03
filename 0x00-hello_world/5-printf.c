43d121628052d79a28a5c46fcc8939da8a47712d
Aly3ldin <alisayed3447300@gmail.com>
2023-03-03 08:02:22 -0800

    Q_T_5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - A program that print a line using  hte printf functions
+ * return :0 (Success)
+ */
+
+ int main  (void)
+ {
+ printf ("with proper grammar, but the outcome is a piece of art,\n");
+
+ return (0);
+ }
+
